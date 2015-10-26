a75d61596394ff0f948b6b02bc4e907732a54d1b
Mike Allred <mikejallred@gmail.com>
2015-10-26 12:53:46 -0400

    Dedupe code, cleanup.

--- bin/sharky.rb
+++ bin/sharky.rb
@@ -1,23 +1,28 @@
 #!/usr/bin/ruby
-require 'open3'
+# purpose: de-dupe filter for tshark probe request listener
+require 'json'
 require 'pp'
 require 'pty'
 
 syntax_tshark = "tshark -n -i mon0 subtype probereq"
 #syntax_tshark = "tshark -n -e wlan.essid -e frame.number -e wlan.addr -i mon0 -T fields -E separator=, -E quote=d -E occurrence=f subtype probereq"
-#syntax_tshark = "tail -f /var/log/syslog"
-#Open3.popen3(syntax_tshark) do |stdout, stderr, status, thread|
-#  puts stdout.read
-#end
-#Open3.popen3(syntax_tshark) {|stdin, stdout, stderr, wait_thread|
-#  puts stdout.read
-#}
+
+$database = {}
 begin
   PTY.spawn(syntax_tshark) do |stdout, stdin, pid|
   begin
     stdout.each { |line|
-      components = line.chomp().split(',')
-      pp components 
+      if line !~ /^\s*\d/
+        next
+      end
+      components = line.chomp().strip().split(',')
+      t_elapsed, mac_source = components[0].split(/\s+/)
+      _, ssid = components[4].split('=')
+      index = "#{mac_source}_#{ssid}"
+      unless $database[index]
+        $database[index] = t_elapsed
+        puts JSON.generate([t_elapsed, mac_source, ssid])
+      end
     }
   rescue Errno::EIO
     puts "bleh"
