ee98c75ae8393e8a5c4bc94db27fb9fe02c164de
Mike Allred <mikejallred@gmail.com>
2017-06-26 02:15:12 +0000

    daily

--- sketch/daily20170619-spiral/solution.rb
+++ sketch/daily20170619-spiral/solution.rb
@@ -2,4 +2,13 @@
 # https://www.reddit.com/r/dailyprogrammer/comments/6i60lr/20170619_challenge_320_easy_spiral_ascension/
 
 class Solution
+  def spiral(n)
+    spiral = ''
+    (1..n**2).each do |f|
+      spiral += "#{f} "
+    end
+    return spiral
+  end
 end
+
+puts Solution.new.spiral(5)
--- sketch/daily20170619-spiral/spec/solution_spec.rb
+++ sketch/daily20170619-spiral/spec/solution_spec.rb
@@ -4,6 +4,15 @@ require_relative '../solution'
 class SolutionTest < Minitest::Test
   def setup
     @solution = Solution.new
+    @tests = {
+      5 => <<-eoc
+ 1  2  3  4 5
+16 17 18 19 6
+15 24 25 20 7
+14 23 22 21 8
+13 12 11 10 9
+      eoc
+    }
   end
   def test_solution
     assert_equal 1, 1 
