07cbf30344186feac14f48a5b6603f71860a2394
Mike Allred <mikejallred@gmail.com>
2017-08-24 21:08:07 -0400

    Incremental.

--- sketch/fizzbuzz.py
+++ sketch/fizzbuzz.py
@@ -4,12 +4,16 @@
 # if the number is divisible by 5, print Buzz
 # if it's divisible by both 3 and 5, print FizzBuzz
 
-for n in range(1, 101):
-    if n % 3 == 0 and n % 5 == 0:
-        print("FizzBuzz")
-    elif n % 3 == 0:
-        print("Fizz")
-    elif n % 5 == 0:
-        print("Buzz")
-    else:
-        print(n)
+def fizzbuzz(start, end):
+    for n in range(start, end):
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")
+        else:
+            print(n)
+
+if __name__ == '__main__':
+    fizzbuzz(1, 101)
