efd7dbbbe15e8ae7008f68ee7860bf016aee4339
Mike Allred <mikejallred@gmail.com>
2021-01-28 10:51:22 -0500

    resilience

--- simcompanies/fetch_ticker.py
+++ simcompanies/fetch_ticker.py
@@ -3,6 +3,17 @@ import os
 from simco_base import *
 from simco_influxdb import *
 
+def push_to_redis():
+    redis_list_push_fifo(redis_list_ticker, json_ticker, 50)
+
+def write_to_influxdb():
+    bucket = "mikejallred's Bucket"
+    client = influxdb_client()
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    for r in list_ticker:
+        influx_line = f'ticker,kind={r["kind"]} price={r["price"]}'
+        ir = write_api.write(bucket, org_influxdb, influx_line)
+
 if __name__ == '__main__':
     r, uri_ticker = request_dict_ticker_from_simco_http() 
     #print(r.content.decode("utf-8"))
@@ -21,15 +32,16 @@ if __name__ == '__main__':
     os.system('echo ""')
 
     logging.debug("pushing to redis")
-    redis_list_push_fifo(redis_list_ticker, json_ticker, 50)
+    try:
+        push_to_redis()
+    except Exception as e:
+        print(e)
 
     logging.debug("pushing to influxdb")
-    bucket = "mikejallred's Bucket"
-    client = influxdb_client()
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-    for r in list_ticker:
-        influx_line = f'ticker,kind={r["kind"]} price={r["price"]}'
-        ir = write_api.write(bucket, org_influxdb, influx_line)
+    try:
+        write_to_influxdb()
+    except Exception as e:
+        print(e)
 
     logging.debug("done")
     print(json_ticker)
